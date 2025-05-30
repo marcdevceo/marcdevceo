e4a3dd8e740c81f0da720d5cbbea2426d8727fe8
marcdevceo <marcdevceo@icloud.com>
2025-05-29 21:50:45 -0400

    chore: changed text in home/HeroSection to buttonText from body

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -13,7 +13,7 @@ export default function HeroSection() {
           color="neutral"
           font="sans"
           weight="medium"
-          size="body"
+          size="buttonText"
         >
           I’m a Full-Stack Product Developer who builds with purpose. I bridge
           the gap between user pain points and elegant solutions. My experience
